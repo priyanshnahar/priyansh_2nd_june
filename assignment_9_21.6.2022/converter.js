21e00504b79b197723f6be8d31eab3aa87cdc841
Priyansh <104319269+priyanshnahar@users.noreply.github.com>
2022-06-21 20:45:10 +0530

    adding converter & oddEven

---
+++ assignment_9_21.6.2022/converter.js
@@ -0,0 +1,8 @@
+var kilometers = 50;
+var miles = kilometers * 0.62137;
+
+console.log(miles);
+
+var miles = 50;
+var kilometers = miles * 1.6;
+console.log(kilometers);
---
+++ assignment_9_21.6.2022/oddEven.js
@@ -0,0 +1,6 @@
+var x = 6;
+if (x % 2 == 0) {
+  console.log(`${x} is an even number`);
+} else {
+  console.log(`${x} is an odd number`);
+}
